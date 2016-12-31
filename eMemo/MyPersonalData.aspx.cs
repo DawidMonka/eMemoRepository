79e3446f910c170062f242702e6fc7094052eb6b
Dawid Monka <dawid.monka@gmail.com>
2016-12-31 17:36:24 +0100

    Add MyPersonalData & UserManagement

---
+++ eMemo/MyPersonalData.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MemoGameSite.Helpers;
+using System.Data;
+using MySql.Data.MySqlClient;
+using eMemo.Helpers;
+
+namespace eMemo
+{
+    public partial class MojeDane : System.Web.UI.Page
+    {
+        private PersonalData personalData;
+        private const string hiddenPass = "*******";
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            personalData = MySession.Current.PersonalData;
+            
+            if (!Page.IsPostBack)
+            {
+                personalData.getAllDataFromDataBase();
+                displayUserData();
+            }
+        }
+
+        private void displayUserData()
+        {
+            nick.Text = personalData.Nick;
+            pass.Text = hiddenPass;
+            name.Text = personalData.Name;
+            surname.Text = personalData.Surname;
+            datepicker.Text = personalData.getBirthDate();
+
+            if (personalData.Sex.Equals(DataBaseConstants.SexValue.Male))
+                male.Checked = true;
+            else
+                female.Checked = true;
+
+            cityList.SelectedValue = personalData.City;
+        }
+
+        protected void updateButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void showPass_Click(object sender, EventArgs e)
+        {
+            if(showPass.Text.Equals("Pokaż hasło"))
+            {
+                pass.Text = personalData.Password;
+                showPass.Text = "Ukryj hasło";
+            }
+            else
+            {
+                pass.Text = hiddenPass;
+                showPass.Text = "Pokaż hasło";
+            }
+        }
+    }
+}
