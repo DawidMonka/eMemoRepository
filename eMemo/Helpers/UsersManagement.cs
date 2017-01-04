948cfe266f20b7dd03ef953595ec7983c114b508
ajemieljanczuk <ajemieljanczuk@gmail.com>
2017-01-04 22:03:05 +0100

    brak wyswietlania Admina

--- eMemo/Helpers/UsersManagement.cs
+++ eMemo/Helpers/UsersManagement.cs
@@ -8,8 +8,12 @@ using MySql.Data.MySqlClient;
 using MemoGameSite.Helpers;
 
 
+
 namespace eMemo.Helpers
 {
+    /// <summary>
+    /// Klasa obsługująca zarządzanie kontami użytkowników przez administratora
+    /// </summary>
     public class UsersManagement
     {
         private DataBaseConnection connection;
@@ -20,19 +24,27 @@ namespace eMemo.Helpers
             connection = new DataBaseConnection();
             
     }
-
+        /// <summary>
+        /// metoda zwraca zbiór danych z użytkowników - nick i data rejestracji z wyłączeniem administratora
+        /// </summary>
+        /// <returns></returns>
         public DataSet getUsers()
         {
             //string cmdtText = "SELECT nick, dataRej FROM uzytkownik order by nick";
-            string cmdtText = String.Format("SELECT {0}, {1} FROM {2} order by {3}",
+            string cmdtText = String.Format("SELECT {0}, {1} FROM {2} where {3} != 'Admin' order by {4}",
                DataBaseConstants.UserTable.Nick,
                DataBaseConstants.UserTable.RegDate,
                DataBaseConstants.UserTableName,
+               DataBaseConstants.UserTable.Nick,
                DataBaseConstants.UserTable.Nick
                 );
             return connection.getDataSetFromDataBase(cmdtText);
         }
-
+        /// <summary>
+        /// metoda usuwająca wyniki użytkownika i dane użytkownika o podanym nicku
+        /// </summary>
+        /// <param name="nick"></param>
+        /// <returns></returns>
         public bool deleteUser(string nick)
         {
 
